7fbabe76699441285966a8f1f5ec413ed4e12155
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-02 04:20:54 +0500

    update 29_09_2025/task6/

--- Semester_1/29_09_2025/task6(Viselnitsa)/game.py
+++ Semester_1/29_09_2025/task6(Viselnitsa)/game.py
@@ -5,92 +5,104 @@ from visualization import *
 
 
 class Result(Enum):
-    CORRECT = 1,
-    INCORRECT = 2,
-    NO_LETTER = 3,
+    CORRECT = 1
+    INCORRECT = 2
+    NO_LETTER = 3
     REPEAT = 4
 
 
 step = TypeVar('step')
 class Game:
-    _word: str
-    _letters: dict[str, bool]
-    _letters_used: List[bool]
-    _steps: List[step]
+    __word: str
+    __letters: dict[str, bool]
+    __letters_used: List[bool]
+    __steps: List[step]
 
     def __init__(self):
-        self._letters = {chr(i): False for i in range(ord('А'), ord('Я') + 1)}
-        self._steps = [step_1(), step_2(), step_3(),
-                       step_4(), step_5(), step_6(),
-                       step_7(), step_8(), step_9(),
-                       step_10(), step_11()]
+        self.__letters = {chr(i): False for i in range(ord('А'), ord('Я') + 1)}
+        self.__steps = [step_1(), step_2(), step_3(),
+                        step_4(), step_5(), step_6(),
+                        step_7(), step_8(), step_9(),
+                        step_10(), step_11()]
 
 
-    def _examination(self, letter: str) -> Result:
-        if letter not in self._letters:
+    def __validate_letter(self, letter: str) -> bool:
+        return len(letter) == 1 and letter in self.__letters
+
+
+    def __examination(self, letter: str) -> Result:
+        if not self.__validate_letter(letter):
             return Result.NO_LETTER
 
-        if self._letters[letter] is True:
+        if self.__letters[letter] is True:
             return Result.REPEAT
 
-        self._letters[letter] = True
+        self.__letters[letter] = True
         flag: int = 0
-        for i in range(len(self._word)):
-            if letter == self._word[i]:
-                self._letters_used[i] = True
+        for i in range(len(self.__word)):
+            if letter == self.__word[i]:
+                self.__letters_used[i] = True
                 flag += 1
 
         return Result.CORRECT if flag != 0 else Result.INCORRECT
 
 
-    def _show_word(self):
-        print('[>] ', end='')
-        for i in range(len(self._word)):
-            if self._letters_used[i]:
-                print(self._word[i], end=' ')
+    def __show_word(self):
+        s = []
+        for i in range(len(self.__word)):
+            if self.__letters_used[i]:
+                s.append(self.__word[i])
             else:
-                print('_', end=' ')
-        print('\n')
+                s.append('_')
+
+        print(f'[>] {" ".join(s)}')
 
 
-    def _is_win(self) -> bool:
-        for l in range(len(self._word)):
-            if self._letters_used[l] is False:
-                return False
+    def __is_win(self) -> bool:
+        return all(self.__letters_used)
 
-        return True
+
+    def __is_lose(self, index_steps: int) -> bool:
+        return index_steps >= len(self.__steps)
 
 
     def play(self):
         print(show_start())
         show_menu()
 
-        self._word = input('[>] Введите слово: ').strip().upper()
-        self._letters_used = [False] * len(self._word)
+        while True:
+            self.__word = input('[>] Введите слово: ').strip().upper()
+            if self.__word != '' and all(ch in self.__letters for ch in self.__word):
+                break
+            else:
+                print('[!] Введите слово корректное слово для русского алфавита.\n')
+
+        self.__letters_used = [False] * len(self.__word)
 
         index_step: int = 0
         while True:
             letter = input('[>] Введите букву: ').strip().upper()
-            result: Result = self._examination(letter)
+            result: Result = self.__examination(letter)
 
             match result:
                 case Result.CORRECT:
                     print('[!] Правильная буква!\n')
                 case Result.INCORRECT:
                     print('[!] Неправильная буква!\n')
-                    print(self._steps[index_step])
+                    print(self.__steps[index_step])
                     index_step += 1
                 case Result.NO_LETTER:
-                    print('[?] В алфавите такой буквы нет')
+                    print('[?] В русском алфавите нет такой буквы.\n')
                 case Result.REPEAT:
-                    print('[!] Буква уже была!')
+                    print('[!] Эта буква уже была использована!\n')
 
-            self._show_word()
+            self.__show_word()
 
-            if self._is_win():
+            if self.__is_win():
                 print('[=] Поздравляем, вы выиграли! :)\n')
                 break
 
-            if index_step == 11:
-                print(f'[=] Вы проиграли ;(\n')
+            if self.__is_lose(index_step):
+                print(f'\n[=] Вы проиграли ;(\n'
+                      f'[>] Загаданное слово: {self.__word}')
                 break
