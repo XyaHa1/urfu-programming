59fa9833a736c9901b457105ff735d9fbe1c0b73
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-05 01:46:16 +0500

    update task2(GeneratePassword).py - update logic choice syms and add menu choice

--- Semester_1/2025-29-09/task4.py
+++ Semester_1/2025-29-09/task4.py
@@ -6,24 +6,54 @@ def generate_password(length: int,
                       use_uppercase: bool,
                       use_lowercase: bool,
                       use_digits: bool,
-                      use_symbols: bool) -> str:
+                      use_symbols: bool,
+                      ) -> str:
     """
     Генерирует пароль заданной длины с использованием указанных символов.
     """
-
-    character_set = ''
+    character_set = []
+    password = []
     if use_uppercase:
+        password.append(random.choice(string.ascii_uppercase))
         character_set += string.ascii_uppercase
     if use_lowercase:
+        password.append(random.choice(string.ascii_lowercase))
         character_set += string.ascii_lowercase
     if use_digits:
+        password.append(random.choice(string.digits))
         character_set += string.digits
     if use_symbols:
+        password.append(random.choice(string.punctuation))
         character_set += string.punctuation
 
-    password = ''.join(random.choice(character_set) for _ in range(length))
+    password += [random.choice(random.choice(character_set)) for _ in range(length - len(password))]
+    random.shuffle(password)
 
-    return password
+    return ''.join(password)
+
+
+def ask(question: str) -> bool:
+    while True:
+        r = input(question).lower()
+        if r == 'y':
+            return True
+        elif r == 'n':
+            return False
+        else:
+            print(
+                "[!] Неверный ответ. Пожалуйста, введите 'y' для подтверждения или 'n' для отмены.")
+
+
+def ask_length(min_length=8) -> int:
+    while True:
+        if (n := input(f"[!] Введите длину пароля (Минимальная длина: {min_length}): ")).isdigit():
+            n = int(n)
+            if n < min_length:
+                print(f"[!] Минимальная длина пароля: {min_length}.")
+            else:
+                return n
+        else:
+            print("[!] Неверный ответ. Пожалуйста, введите число.")
 
 
 def main():
@@ -32,11 +62,30 @@ def main():
     """
 
     print("Генератор сложных паролей.")
-    length = int(input("[!] Введите длину пароля: "))
-    flag_upper = input("[!] Использовать заглавные буквы? (y/n): ").lower() == 'y'
-    flag_lower = input("[!] Использовать строчные буквы? (y/n): ").lower() == 'y'
-    flag_digits = input("[!] Использовать цифры? (y/n): ").lower() == 'y'
-    flag_symbols = input("[!] Использовать спецсимволы? (y/n): ").lower() == 'y'
+    length = ask_length()
+    count_true = 0
+
+    flag_upper: bool
+    flag_lower: bool
+    flag_digits: bool
+    flag_symbols: bool
+
+    while count_true < 2:
+        flag_upper = ask("[!] Использовать заглавные буквы? (y/n): ")
+        if flag_upper:
+            count_true += 1
+        flag_lower = ask("[!] Использовать строчные буквы? (y/n): ")
+        if flag_lower:
+            count_true += 1
+        flag_digits = ask("[!] Использовать цифры? (y/n): ")
+        if flag_digits:
+            count_true += 1
+        flag_symbols = ask("[!] Использовать спецсимволы? (y/n): ")
+        if flag_symbols:
+            count_true += 1
+        if count_true < 2:
+            print('[>] Необходимо выбрать хотя бы два типа символов.')
+
 
     password = generate_password(length, flag_upper, flag_lower, flag_digits, flag_symbols)
 
