f5c500563d27959ccae406935e89ae8a1de1e3ff
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-06 22:38:46 +0500

    update task2.py - final vers

--- Semester_1/lesson3/task2.py
+++ Semester_1/lesson3/task2.py
@@ -1,81 +1,102 @@
 import string
+from typing import Set, Dict, Tuple
 
 
-def check_letter(ch: str, s: set) -> int:
-    if ch in s:
-        return 1
-    return 0
+def select_sets_of_lang(lang: str) -> Tuple[Set[str], Set[str]]:
+    vowels: Set[str] = set()
+    cons: Set[str] = set()
 
+    if 'ru' in lang:
+        vowels.update({'а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'})
+        cons.update({'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н',
+                'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь'})
+    if 'eng' in lang:
+        vowels.update({'a', 'e', 'i', 'o', 'u', 'y'})
+        cons.update({'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
+                'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z',})
 
-def check_symbol(ch: str, vowels: set, cons: set) -> bool:
-    return ch in vowels or ch in cons or ch in string.digits
+    return vowels, cons
 
 
 def analys(text: str, lang: str):
-    vowels: set
-    cons: set
+    vowels, cons = select_sets_of_lang(lang)
 
-    if lang == 'ru':
-        vowels = {'а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'}
-        cons = {'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш',}
-    elif lang == 'eng':
-        vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
-        cons = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z',}
-
-    result: dict = {
+    result: Dict = {
         "vowels": 0,
         "cons": 0,
         "whitespace": 0,
-        "frequent symbols (top-3)": [],
+        "top_3_frequent_symbols": [],
         "count_words": 0,
     }
 
-    letters = {}
+    all_chars: Dict[str, int] = {}
     i = 0
     while i < len(text):
         ch = text[i].lower()
+
         if ch.isspace():
-            result['whitespace'] += check_letter(ch, string.whitespace)
-        elif ch not in string.punctuation:
-            while i < len(text) and check_symbol(text[i].lower(), vowels, cons):
+            result['whitespace'] += 1
+            i += 1
+
+        elif ch in string.punctuation:
+            all_chars[ch] = all_chars.get(ch, 0) + 1
+            i += 1
+
+        elif ch.isalnum():
+            while i < len(text) and text[i].isalnum() and not text[i].isspace():
                 ch = text[i].lower()
-                result['vowels'] += check_letter(ch, vowels)
-                result['cons'] += check_letter(ch, cons)
-                letters[ch] = letters.get(ch, 0) + 1
+
+                if ch in vowels or ch in cons:
+                    if ch in vowels:
+                        result['vowels'] += 1
+                    elif ch in cons:
+                        result['cons'] += 1
+
+                ch = text[i]
+                all_chars[ch] = all_chars.get(ch, 0) + 1
                 i += 1
 
             result['count_words'] += 1
-        i += 1
 
-    result['frequent symbols (top-3)'] = sorted(letters.items(), key=lambda x: x[1], reverse=True)[:3]
+        else:
+            i += 1
+
+    result['top_3_frequent_symbols'] = sorted(all_chars.items(), key=lambda x: x[1], reverse=True)[:3]
 
     return result
 
 
-def ans_lang():
+def select_lang():
     while True:
-        l = input("[>] Введите язык текста (ru/eng): ")
-        if l == 'ru' or l == 'eng':
+        l = input("[>] Провести анализ текста на основе (ru, eng, ru/eng): ")
+        if l == 'ru' or l == 'eng' or l == 'ru/eng':
             return l
         else:
-            print("[W] Неверный язык. Введите 'ru' или 'eng'")
+            print("[W] Неверный язык. Введите 'ru', 'eng' или 'ru/eng'")
 
 
-def main():
-    lang = ans_lang()
+def start():
+    lang = select_lang()
     text = input("[>] Введите текст: ")
+
     res = analys(text, lang)
-    print(res, sep='\n')
+
+    vowels = res['vowels']
+    cons = res['cons']
+    whitespace = res['whitespace']
+    top_3 = res['top_3_frequent_symbols']
+    count_words = res['count_words']
+
+    print(f"\n[O] Результат анализа на основе "
+          f"{'русского' if lang == 'ru' else 'английского' 
+          if lang == 'eng' else 'русского и английского'} набора: \n"
+          f"    Гласных букв: {vowels}\n"
+          f"    Согласных букв: {cons}\n"
+          f"[O] Общие данные: \n"
+          f"    Топ-3 символа: {top_3}\n"
+          f"    Количество слов: {count_words}\n"
+          f"    Пробелов: {whitespace}\n")
 
 
 if __name__ == '__main__':
-    # assert analys("Живи так, как будто будешь жить вечно, и ты будешь жить для всех. ", 'ru') == {'vowels': 18,
-    #                                                                'cons': 42,
-    #                                                                'whitespace': 13,
-    #                                                                'frequent words (top-3)': [('жиnm', 2), ('будто', 2), ('будешь', 2)],
-    #                                                                'count_words': 13}
-    print(analys("Живи так, как будто будешь жить вечно, и ты будешь жить для всех. ", 'ru'))
-
-
-
-
+    start()
