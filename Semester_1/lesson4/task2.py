ff3954ad1538f3b8a52aa0093d63f6b2e9b4cb25
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-13 17:37:29 +0500

    create ./lesson4/task2.py

---
+++ Semester_1/lesson4/task2.py
@@ -0,0 +1,30 @@
+def correct_brackets(exp: str):
+    brackets = 0
+    for ch in exp:
+        if ch == '(':
+            brackets += 1
+        elif ch == ')':
+            brackets -= 1
+        if brackets < 0:
+            return False
+
+    return brackets == 0
+
+def test():
+    assert correct_brackets('(())') is True
+    assert correct_brackets('((()))') is True
+    assert correct_brackets('()') is True
+    assert correct_brackets('((2+2)2)') is True
+    assert correct_brackets('((2+2)2)(') is False
+    assert correct_brackets('((2+2)2))(') is False
+    assert correct_brackets(')(') is False
+    assert correct_brackets('') is True
+    assert correct_brackets(')') is False
+    assert correct_brackets('(') is False
+
+
+if __name__ == '__main__':
+    test()
+    while True:
+        exp = input('[>] Введите выражение: ')
+        print(f'[!] Правильность скобок: {correct_brackets(exp)}')
