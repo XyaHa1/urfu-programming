b9a85608e05ede84611527f5c82ffc1151aca342
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-13 18:00:35 +0500

    pep8 ./lesson4/

--- Semester_1/lesson4/task1.py
+++ Semester_1/lesson4/task1.py
@@ -2,16 +2,16 @@ from math import log10, floor
 
 
 def func(n: int):
-    all_length = 0 # длина всех чисел
-    for i in range(1, 2 ** 31 - 1):
-        all_length += floor(log10(i) + 1) # вычисляем длину числа
+    all_length = 0  # длина всех чисел
+    for i in range(1, 2**31 - 1):
+        all_length += floor(log10(i) + 1)  # вычисляем длину числа
         if n < all_length:
             ind = all_length - n
             num = str(i)[-ind]
             return num
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
-        ind = int(input('[>] Введите индекс символа: '))
+        ind = int(input("[>] Введите индекс символа: "))
         print(func(ind))
--- Semester_1/lesson4/task3.py
+++ Semester_1/lesson4/task3.py
@@ -8,12 +8,9 @@ class Side(Enum):
     LEFT_RIGHT = 3
 
 
-def side_of_string(new_s: List[str], 
-                   target: str, 
-                   count_target_symbol: int, 
-                   side: Side):
-    target_str: str  = target * count_target_symbol
-    new_string: str = ''.join(new_s)
+def side_of_string(new_s: List[str], target: str, count_target_symbol: int, side: Side):
+    target_str: str = target * count_target_symbol
+    new_string: str = "".join(new_s)
 
     if side == Side.LEFT:
         new_string = target_str + new_string
@@ -21,9 +18,8 @@ def side_of_string(new_s: List[str],
         new_string = new_string + target_str
     elif side == Side.LEFT_RIGHT:
         new_string = target_str + new_string + target_str
-    
+
     return new_string
-        
 
 
 def permutation_symbols(s: str, target: str, side: Side):
@@ -34,16 +30,14 @@ def permutation_symbols(s: str, target: str, side: Side):
             count_target_symbol += 1
         else:
             new_string.append(ch)
-        
+
     return side_of_string(new_string, target, count_target_symbol, side)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
-        s = input('[>] Введите строку: ')
-        target = input('[>] Введите символ: ')
-        print('1. Левый\n'
-              '2. Правый\n'
-              '3. Левый и Правый')
-        side = Side(int(input('[>] Выберите сторону: ')))
+        s = input("[>] Введите строку: ")
+        target = input("[>] Введите символ: ")
+        print("1. Левый\n" "2. Правый\n" "3. Левый и Правый")
+        side = Side(int(input("[>] Выберите сторону: ")))
         print(permutation_symbols(s, target, side))
