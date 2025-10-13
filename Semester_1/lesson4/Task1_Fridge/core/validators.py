3f9f43d836a79f3ca5eaa37163f76ab6a7ad5326
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-14 00:27:38 +0500

    create ./validators.py: создал валидаторы для вводимых данных

---
+++ Semester_1/lesson4/Task1_Fridge/core/validators.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from .exceptions import DateFormatError, CountItemsError, AmountError
+from ._config import DATE_FORMAT
+
+
+class Validator:
+
+    @staticmethod
+    def valid_amount(amount: str):
+        if amount.count('.') > 1:
+            raise AmountError()
+
+        if amount.count(',') > 0:
+            raise AmountError()
+
+        for ch in amount:
+            if not ch.isdigit():
+                if ch != '.':
+                    raise AmountError()
+        return True
+
+
+    @staticmethod
+    def valid_date(date_str: str):
+        try:
+            datetime.strptime(date_str, DATE_FORMAT)
+            return True
+        except ValueError:
+            raise DateFormatError()
+
+    @staticmethod
+    def valid_count_items(items: str):
+        if len(items) == 0:
+            raise CountItemsError()
+
+        items = items.split(',')
+        if len(items) < 2:
+            raise CountItemsError()
+
+        return True
