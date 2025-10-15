f8bdc42536044c47adb344aae22dcd36ce15d003
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-16 02:26:33 +0500

    update ./fridge.py: переписал проверка на amount, дополнил проверку на количество элементов

--- Semester_1/lesson4/Task1_Fridge/core/validators.py
+++ Semester_1/lesson4/Task1_Fridge/core/validators.py
@@ -1,44 +1,52 @@
 from datetime import datetime
 from .exceptions import DateFormatError, CountItemsError, AmountError
 from ._config import DATE_FORMAT
+from decimal import Decimal, InvalidOperation
 
 
 class Validator:
 
     @staticmethod
     def valid_amount(amount: str):
+        amount = amount.strip()
         if amount.count('.') > 1:
             raise AmountError()
 
         if amount.count(',') > 0:
             raise AmountError()
 
-        for ch in amount:
-            if not ch.isdigit():
-                if ch != '.':
-                    raise AmountError()
+        if len(amount.split('.')) > 2:
+            raise AmountError()
 
-        return True
+        if amount.startswith('.') or amount.endswith('.'):
+            raise AmountError()
 
+        try:
+            d = Decimal(amount)
+        except InvalidOperation:
+            raise AmountError()
+
+        if d < 0:
+            raise AmountError()
 
     @staticmethod
     def valid_date(date_str: str | None):
-        if date_str is None:
-            return
-
         try:
             datetime.strptime(date_str, DATE_FORMAT)
-            return True
         except ValueError:
             raise DateFormatError()
 
     @staticmethod
     def valid_count_items(items: str):
-        if len(items) == 0:
+        if len(items.strip()) == 0:
             raise CountItemsError()
 
-        items = items.split(',')
-        if len(items) < 2:
+        items = [item.strip() for item in items.split(',')]
+        items = [item for item in items if len(item) > 0]
+
+
+        if len(items) < 2 or len(items) > 3:
             raise CountItemsError()
 
-        return True
+        if len(items[0]) == 0 or len(items[1]) == 0:
+            raise CountItemsError()
