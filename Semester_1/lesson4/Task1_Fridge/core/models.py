464855c1ef9d065edf0419762a1fcc16e7e5f7d5
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-16 05:11:28 +0500

    create ./models.py: вынес Product из fridge.py, создал классы для title, amount, date

---
+++ Semester_1/lesson4/Task1_Fridge/core/models.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from datetime import date, datetime
+from decimal import Decimal
+
+from ._config import DATE_FORMAT
+
+
+class Title:
+    def __init__(self, title: str):
+        self._title: str = self.__to_format(title)
+
+    def __str__(self) -> str:
+        return self.title
+
+    @property
+    def title(self):
+        return self._title
+
+
+class Amount:
+    def __init__(self, amount: str):
+        self._amount: Decimal = Decimal(amount)
+
+    def __str__(self) -> Decimal:
+        return self._amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+
+class Date:
+    def __init__(self, date: str | None):
+        if date is not None:
+            self._date: date = datetime.strptime(date, DATE_FORMAT).date()
+        else:
+            self._date = None
+
+    def __str__(self) -> date | None:
+        return self._date
+
+    @property
+    def date(self):
+        return self._date
+
+
+@dataclass
+class Product:
+    title: Title
+    amount: Amount
+    expiration_date: Date
