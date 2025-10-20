24d7e09fc4e48903e389773c0617c811f3f04290
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-20 17:23:17 +0500

    update lesson5/task1.py: добавил новые свойства

--- Semester_1/lesson5/task1.py
+++ Semester_1/lesson5/task1.py
@@ -1,9 +1,11 @@
 class Book:
 
-    def __init__(self, title, author, year):
+    def __init__(self, title, author, year, price, publisher):
         self.title = title
         self.author = author
         self.year = year
+        self.price = price
+        self.publisher = publisher
 
     def info(self):
         return f"Title: {self.title}, author: {self.author}, year: {self.year}"
