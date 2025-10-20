a20d368e70f012314880204c2e11e9925903b0af
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-20 17:24:04 +0500

    create lesson5/task3.py: класс вектор

---
+++ Semester_1/lesson5/task3.py
@@ -0,0 +1,27 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.x + other, self.y + other)
+
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        return Vector(self.x - other, self.y - other)
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x * other.x, self.y * other.y)
+        return Vector(self.x * other, self.y * other)
+
+    def __str__(self):
+        return f'Vector({self.x}, {self.y})'
+
+
+if __name__ == '__main__':
+    v1 = Vector(1, 2)
+    v2 = Vector(3, 4)
