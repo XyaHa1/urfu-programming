b411f02c0ff7e4aa3c642931343ef0bb5ddea103
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-23 03:56:56 +0500

    update /potion.py: изменил логику add, добавил геттеры

--- Semester_1/lesson5/KnightAndDragon/consumables/potion.py
+++ Semester_1/lesson5/KnightAndDragon/consumables/potion.py
@@ -1,48 +1,43 @@
-from abc import ABC
-
-
-class Potion(ABC):
-    def __init__(self, name: str, max_count: int, solute: int):
+class Potion:
+    def __init__(self, name: str, max_count: int, value: int):
         self._name: str = name
         self._max_count: int = max_count
         self._current_count: int = max_count
-        self._solute: int = solute
+        self._value: int = value
+
+    @property
+    def max_count(self):
+        return self._max_count
+
+    @property
+    def current_count(self):
+        return self._current_count
+
+    @property
+    def value(self):
+        return self._value
 
     def __str__(self):
-        return f"{self._name} ({self._current_count}/{self._max_count})"
+        return f"{self._name}"
 
-    def add(self, count=1) -> None:
-        if self._current_count < self._max_count:
-            self._current_count = min(self._current_count + count, self._max_count)
+    def add(self, count=1) -> int:
+        added = min(count, self._max_count - self._current_count)
+        self._current_count += added
+        return added
 
     def use(self) -> int:
         self._current_count -= 1
-        return self._solute
+        return self._value
 
     def can_use(self) -> bool:
         return self._current_count > 0
 
 
 class HealingPotion(Potion):
-    def __init__(self, name, max_count, healing):
-        super().__init__(name, max_count, healing)
+    def __init__(self, name: str, max_count: int, healing_amount: int):
+        super().__init__(name, max_count, healing_amount)
 
 
 class ManaPotion(Potion):
-    def __init__(self, name, max_count, mana):
-        super().__init__(name, max_count, mana)
-
-
-class PotionManager:
-    def __init__(self, *potions):
-        self._potions = dict(potions)
-
-    def use(self, potion_name) -> int:
-        if self._potions[potion_name].can_use():
-            return self._potions[potion_name].use()
-
-    def add(self, potion_name, count=1) -> None:
-        self._potions[potion_name].add(count)
-
-    def can_use(self, potion_name) -> bool:
-        return self._potions[potion_name].can_use()
+    def __init__(self, name: str, max_count: int, mana_amount: int):
+        super().__init__(name, max_count, mana_amount)
