959dc268d3e97fa6d2ba1c28a1c6aa5d66418a09
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-23 03:58:54 +0500

    update manager.py: добавил метод add(). К дополнению прошлого: создал класс PotionManager

--- Semester_1/lesson5/KnightAndDragon/consumables/manager.py
+++ Semester_1/lesson5/KnightAndDragon/consumables/manager.py
@@ -14,3 +14,12 @@ class PotionManager:
             return potion.use()
         print(f"*({potion}) Закончилось, не могу им пользоваться*")
         return 0
+
+    def add(self, potion_name: str, count: int = 1) -> None:
+        potion = self._potions.get(potion_name)
+        added = potion.add(count)
+        if added > 0:
+            print(
+                f"*({self._potions[potion_name]}) Отлично, теперь у меня {self._potions[potion_name].current_count} из {self._potions[potion_name].max_count}*")
+            return
+        print(f"*({self._potions[potion_name]}) Не могу положить, нет места*")
