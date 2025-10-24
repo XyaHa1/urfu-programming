4a649d7421dfdacfcd4bbe29066ce770d700f12d
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-25 00:01:41 +0500

    update /weapon.py: написал логику крит. шанса

--- Semester_1/lesson5/KnightAndDragon/weapons/weapon.py
+++ Semester_1/lesson5/KnightAndDragon/weapons/weapon.py
@@ -1,14 +1,21 @@
+import random
 from abc import ABC
 
 
 class Weapon(ABC):
-    def __init__(self, name, damage):
+    def __init__(self, name: str, damage: int, crit_chance: float):
         self._name: str = name
         self._damage: int = damage
+        self._crit_chance: float = crit_chance
+
+    def __str__(self) -> str:
+        return self._name
 
     @property
-    def damage(self):
+    def damage(self) -> int:
         return self._damage
 
-    def __str__(self):
-        return self._name
+    def multiplier(self) -> float:
+        if random.uniform(0, 1) <= self._crit_chance:
+            return round(random.uniform(1, 2), 2)
+        return 1.0
