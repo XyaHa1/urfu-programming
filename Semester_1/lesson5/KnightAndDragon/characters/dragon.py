4645acc15168ae5f168ee1c1e39f6cd9c1617079
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:10:26 +0500

    delete files

--- Semester_1/lesson5/KnightAndDragon/characters/dragon.py
+++
@@ -1,9 +0,0 @@
-from .character import Character
-
-
-class Dragon(Character):
-    def __init__(self, name, health, damage, shild):
-        super().__init__(name, health, damage, shild)
-
-    def attack(self, enemy: Character):
-        pass
