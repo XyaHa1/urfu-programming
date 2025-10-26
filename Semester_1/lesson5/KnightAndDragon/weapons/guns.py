69ae59c9642ab0562487b08202ee2b28e7ecf899
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:19:05 +0500

    update guns.py: добавил типизация оружия для вывода сообщений

--- Semester_1/lesson5/KnightAndDragon/weapons/guns.py
+++ Semester_1/lesson5/KnightAndDragon/weapons/guns.py
@@ -1,15 +1,25 @@
-from weapon import Weapon
+from ..weapons import Weapon
 
 
 class Bow(Weapon):
     def __init__(self, name, damage, crit_chance: float):
         super().__init__(name, damage, crit_chance)
 
+    def type(self):
+        return "ranged"
+
+
 class Crossbow(Weapon):
     def __init__(self, name, damage, crit_chance: float):
         super().__init__(name, damage, crit_chance)
+
+    def type(self):
+        return "ranged"
 
 
 class RangedStaff(Weapon):
     def __init__(self, name, damage, crit_chance: float):
         super().__init__(name, damage, crit_chance)
+
+    def type(self):
+        return "ranged"
