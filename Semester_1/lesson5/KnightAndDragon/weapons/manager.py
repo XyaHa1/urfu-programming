2fcb9f87711820da0be6a68e839dd94199c8963c
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:16:59 +0500

    update /weapons/manager.py: документировал, выводит рандом фразы

--- Semester_1/lesson5/KnightAndDragon/weapons/manager.py
+++ Semester_1/lesson5/KnightAndDragon/weapons/manager.py
@@ -1,28 +1,51 @@
+import random
 from typing import Dict
 
-from weapon import Weapon
+from ..weapons import Weapon
 
 
 class WeaponManager:
-    def __init__(self, weapons: Dict[str, Weapon]):
+    NARRATOR_MELEE_PHRASES = [
+        "'{user}' взмахнул '{weapon}' — и воздух рассёкся от силы удара. Враг пошатнулся, получив {damage:.0f} урона.",
+        "'{weapon}' сверкнул в свете заката. Враг почувствовал боль — {damage:.0f} жизненных сил ушло в никуда.",
+        "Без крика, без предупреждения — '{user}' нанёс удар. Противник теряет {damage:.0f} здоровья.",
+        "Сталь зазвенела, встречая плоть. Противник получил {damage:.0f} урона — и это лишь начало.",
+    ]
+
+    NARRATOR_RANGED_PHRASES = [
+        "Снаряд '{user}' сорвалась с '{weapon}' — и в следующее мгновение вонзилась во врага. Урон: {damage:.0f}.",
+        "Снаряд вылетел из '{weapon}' '{user}' и нашёл свою цель. Враг почувствовал, как боль пронзает тело.",
+        "На расстоянии '{user}' смертоносен. Злодей получил {damage:.0f} урона — и не видел, откуда пришла смерть.",
+    ]
+
+    def __init__(self, name_user: str, weapons: Dict[str, Weapon], custom_print=None):
+        self._name_user = name_user
         self._weapons = weapons
         self._current_weapon_name: str = next(iter(weapons.keys()))
+        self._custom_print = custom_print
 
     def get_current_weapon(self) -> Weapon:
+        """Выдает текущее оружие."""
         return self._weapons[self._current_weapon_name]
+
+    def get_damage_weapon(self) -> int:
+        """Выдает урон текущего оружия."""
+        return self._weapons[self._current_weapon_name].damage
 
     def switch_to(self, name: str) -> None:
         """Переключает оружие и выводит сообщение."""
         if name not in self._weapons:
-            print(f"*У меня нет оружия '{name}'*")
+            self._custom_print(f"У {self._name_user} нет оружия '{name}'.")
             return
 
         if name == self._current_weapon_name:
-            print(f"*Я уже использую {name}.*")
+            self._custom_print(f"{self._name_user} уже использует {name}.")
             return
 
         self._current_weapon_name = name
-        print(f"*Я использую {name}*")
+        self._custom_print(
+            f"Теперь '{self._name_user}' использует {name}. Врагу следует бежать!"
+        )
 
     def damage(self) -> float:
         """
@@ -38,4 +61,16 @@ class WeaponManager:
         base = weapon.damage
         damage = base * multiplier
         print(f"[Урон] Вы нанесли {damage:.0f} единиц урона!")
+        if weapon.type() == "melee":
+            self._custom_print(
+                random.choice(self.NARRATOR_MELEE_PHRASES).format(
+                    user=self._name_user, damage=damage, weapon=str(weapon)
+                )
+            )
+        elif weapon.type() == "ranged":
+            self._custom_print(
+                random.choice(self.NARRATOR_RANGED_PHRASES).format(
+                    user=self._name_user, damage=damage, weapon=str(weapon)
+                )
+            )
         return damage
