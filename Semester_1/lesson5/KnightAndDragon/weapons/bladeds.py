cec7097e221a807bb83ccaacedd7ad0672f20a8a
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:18:49 +0500

    update bladeds.py: добавил типизация оружия для вывода сообщений

--- Semester_1/lesson5/KnightAndDragon/weapons/bladeds.py
+++ Semester_1/lesson5/KnightAndDragon/weapons/bladeds.py
@@ -1,26 +1,41 @@
-from weapon import Weapon
+from ..weapons import Weapon
 
 
 class Sword(Weapon):
     def __init__(self, name: str, damage: int, crit_chance: float):
         super().__init__(name, damage, crit_chance)
 
+    def type(self):
+        return "melee"
+
 
 class Axe(Weapon):
     def __init__(self, name: str, damage, crit_chance: float):
         super().__init__(name, damage, crit_chance)
+
+    def type(self):
+        return "melee"
 
 
 class Knife(Weapon):
     def __init__(self, name: str, damage, crit_chance: float):
         super().__init__(name, damage, crit_chance)
 
+    def type(self):
+        return "melee"
+
 
 class Spear(Weapon):
     def __init__(self, name: str, damage, crit_chance: float):
         super().__init__(name, damage, crit_chance)
 
+    def type(self):
+        return "melee"
+
 
 class MeleeStaff(Weapon):
     def __init__(self, name: str, damage, crit_chance: float):
         super().__init__(name, damage, crit_chance)
+
+    def type(self):
+        return "melee"
