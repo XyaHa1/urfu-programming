e1dcfe2bd88803f4e33cc8e65fdf56fa82bf599b
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:21:11 +0500

    update /character.py: интеграция с эффектами и способностями

--- Semester_1/lesson5/KnightAndDragon/characters/character.py
+++ Semester_1/lesson5/KnightAndDragon/characters/character.py
@@ -1,13 +1,22 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from typing import List
+
+from ..consumables import EffectManager
+from ..consumables.effects import Effect
 
 
 class Character(ABC):
-    def __init__(self, name: str, max_health: int, health: int, shield: int, damage: int):
+    def __init__(
+            self, name: str, max_health: int, health: int, shield: int, effect_manager: EffectManager
+    ):
         self._name: str = name
         self._max_health: int = max_health
         self._health: int = health
         self._shield: int = shield
-        self._damage: int = damage
+        self._active_effect: List[Effect] = []
+        self._effect_manager: EffectManager = effect_manager
 
     def __str__(self) -> str:
         return self._name
@@ -25,30 +34,47 @@ class Character(ABC):
         self._health = min(self._max_health, self._health + health)
 
     @property
-    def shield(self) -> int:
+    def shield(self) -> float:
         return self._shield
-
-    @property
-    def damage(self) -> int:
-        return self._damage
 
     def is_alive(self) -> bool:
         return self._health > 0
 
-    def take_damage(self, damage: int) -> None:
+    def take_damage(self, damage: float) -> None:
+        for effect in self._active_effect:
+            if str(effect) == "Щит":
+                effect.consume()
+                self._effect_manager.on_effect(self, effect)
+                self._active_effect.remove(effect)
+                return
+
         if self._shield > 0:
             preprocessing_damage = self._shield - damage / 2
             if preprocessing_damage < 0:
                 normalize_damage = abs(preprocessing_damage) * 2
                 self._health = self._health - normalize_damage
-            self._shield = max(preprocessing_damage, 0)
+            self._shield = round(max(preprocessing_damage, 0), 2)
         else:
-            self._health = max(self._health - damage, 0)
+            self._health = round(max(self._health - damage, 0), 2)
+
+    def add_effect(self, effect: Effect) -> None:
+        for effect_exist in self._active_effect:
+            if str(effect_exist) == str(effect):
+                effect_exist.description = effect.description
+                return
+        self._active_effect.append(effect)
+
+    def process_effect(self) -> None:
+        for effect in reversed(self._active_effect):
+            effect.tick(self)
+            self._effect_manager.on_effect(self, effect)
+            if effect.description <= 0:
+                self._active_effect.remove(effect)
 
     @abstractmethod
-    def attack(self, enemy: 'Character') -> None:
+    def attack(self, enemy: "Character") -> None:
         pass
 
     @abstractmethod
-    def use_ability(self, enemy: 'Character') -> None:
+    def use_ability(self, enemy: "Character") -> None:
         pass
