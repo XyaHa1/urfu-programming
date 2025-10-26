4dd34bc7c31e4beeb4d2d3813b05f01474586278
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:09:27 +0500

    create /bleeding_effect.py: эффект кровотечения

---
+++ Semester_1/lesson5/KnightAndDragon/consumables/effects/bleeding_effect.py
@@ -0,0 +1,10 @@
+from ..effects import Effect
+
+
+class BleedEffect(Effect):
+    def __init__(self, name, damage, duration):
+        super().__init__(name, duration)
+        self._damage = damage
+
+    def apply(self, target) -> None:
+        target.take_damage(self._damage)
