6c247cc864a32e8e36c5cf49f28fb0ecb565e36a
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:11:08 +0500

    update /effects/__init__.py: импорты классов

---
+++ Semester_1/lesson5/KnightAndDragon/consumables/effects/__init__.py
@@ -0,0 +1,6 @@
+from .bleeding_effect import BleedEffect
+from .burning_effect import BurnEffect
+from .effect import Effect
+from .shielding_effect import ShieldingEffect
+
+__all__ = ["Effect", "BleedEffect", "BurnEffect", "ShieldingEffect"]
