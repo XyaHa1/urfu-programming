755dafde0faf51f678880d5e36b8a92aff9f7a02
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-26 23:08:59 +0500

    create /burning_effect.py: класс для фаерболла

---
+++ Semester_1/lesson5/KnightAndDragon/consumables/effects/burning_effect.py
@@ -0,0 +1,10 @@
+from ..effects import Effect
+
+
+class BurnEffect(Effect):
+    def __init__(self, name, damage, duration):
+        super().__init__(name, duration)
+        self.damage_per_turn = damage
+
+    def apply(self, target) -> None:
+        target.take_damage(self.damage_per_turn)
