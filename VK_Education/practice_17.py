1c4b84dab30be09c0860d04e0ce8021b46dbbfeb
Polovinkin Artem <polovinkin2006@list.ru>
2025-10-30 03:31:16 +0500

    create practice_17.py

---
+++ VK_Education/practice_17.py
@@ -0,0 +1,10 @@
+def filter(func, seq):
+    for x in seq:
+        if func(x):
+            yield x
+
+
+func_in, seq_in = eval(input()), eval(input())
+
+for x in filter(func_in, seq_in):
+    print(x)
